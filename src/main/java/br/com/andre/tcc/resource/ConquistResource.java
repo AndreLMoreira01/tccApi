a3d20f28729db29b1a43ea1006ea3d1bccb71b93
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-08 19:39:35 -0300

    Conquista que faz sentido

--- src/main/java/br/com/andre/tcc/resource/ConquistResource.java
+++ src/main/java/br/com/andre/tcc/resource/ConquistResource.java
@@ -5,11 +5,13 @@ import br.com.andre.tcc.model.Historia;
 import br.com.andre.tcc.repository.ConquistaRepository;
 import br.com.andre.tcc.service.ConquistaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/conquistas")
@@ -23,4 +25,29 @@ public class ConquistResource {
 
     @GetMapping("/todos")
     public List<Conquista> listarTodasConquistar() { return conquistaService.listarTodasConquistar(); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Conquista> buscarPeloId(@PathVariable Long id){
+        Optional<Conquista> conquista = conquistaRepository.findById(id);
+        return conquista.isPresent() ? ResponseEntity.ok(conquista.get()) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long id){
+        conquistaRepository.deleteById(id);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Conquista> criar(@RequestBody Conquista conquista, HttpServletResponse response) {
+        Conquista conquistaSalvo = conquistaService.salvar(conquista);
+        return ResponseEntity.status(HttpStatus.CREATED).body(conquistaSalvo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Conquista> atualizar(@PathVariable Long id, @RequestBody Conquista conquista){
+        Conquista conquistaSalvo = conquistaService.atualizar(id, conquista);
+        return ResponseEntity.ok(conquistaSalvo);
+    }
+
 }
--- src/main/java/br/com/andre/tcc/service/ConquistaService.java
+++ src/main/java/br/com/andre/tcc/service/ConquistaService.java
@@ -2,10 +2,12 @@ package br.com.andre.tcc.service;
 
 import br.com.andre.tcc.model.Conquista;
 import br.com.andre.tcc.repository.ConquistaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ConquistaService {
@@ -17,6 +19,26 @@ public List<Conquista> listarTodasConquistar(){
     return conquistaRepository.findAll();
 }
 
+    public Conquista salvar(Conquista  conquista){
+        return conquistaRepository.save(conquista);
+    }
+
+    public Conquista atualizar(Long id, Conquista conquista) {
+        Conquista conquistaSalvo = buscarConquistaExistente(id);
+
+        BeanUtils.copyProperties(conquista, conquistaSalvo, "id");
+
+        return conquistaRepository.save(conquistaSalvo);
+    }
+    
+    private Conquista buscarConquistaExistente(Long id) {
+        Optional<Conquista> conquistaSalvo = conquistaRepository.findById(id);
+        if (!conquistaSalvo.isPresent()){
+            throw new IllegalArgumentException();
+        }
+
+        return conquistaSalvo.get();
+    }
 
 
 }
