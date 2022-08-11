a3959cd49074f677fc909c6422296c90a3b2c3b0
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-11 14:37:08 -0300

    Delete V003__Criar_Tabela_Jogo.sql

--- V003__Criar_Tabela_Jogo.sql
+++
@@ -1,9 +0,0 @@
- create table jogo(
-	id_historia Bigint not null primary key auto_increment,
-	nome VARCHAR(100),
-	descricao VARCHAR(150),
-    conquista_id int not null
- );
-
- insert into jogo(nome, descricao, conquista_id) values ('Jogo Dos Significados', 'Desc', 3);
- insert into jogo(nome, descricao, conquista_id) values ('Genius', 'Desc', 4);
