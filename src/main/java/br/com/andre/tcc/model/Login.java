73e7922e84ba656e0fcd0a3ce33617d59e0a6ffe
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-18 09:42:38 -0300

    Get and set login

--- src/main/java/br/com/andre/tcc/model/Login.java
+++ src/main/java/br/com/andre/tcc/model/Login.java
@@ -1,6 +1,7 @@
 package br.com.andre.tcc.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name="login")
@@ -12,4 +13,42 @@ public class Login {
 
     private String username;
 
+    private String senha;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Login login = (Login) o;
+        return id == login.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
---
+++ src/main/resources/db/migration/V005__Criar_Tabela_Login.sql
@@ -0,0 +1,5 @@
+ create table login(
+	id_login Bigint not null primary key auto_increment,
+	username VARCHAR(100),
+	senha VARCHAR(100)
+ );
