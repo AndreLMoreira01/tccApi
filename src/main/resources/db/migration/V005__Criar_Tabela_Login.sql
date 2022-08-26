e0a2c99bfbf1ca6b2969596c52fab6cca5434acb
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-26 09:25:54 -0300

    Delete V005__Criar_Tabela_Login.sql

--- src/main/resources/db/migration/V005__Criar_Tabela_Login.sql
+++
@@ -1,8 +0,0 @@
- create table login(
-	id_login Bigint not null primary key auto_increment,
-	username VARCHAR(100),
-	senha VARCHAR(100)
- );
-
-insert into login(username, senha) values ('JoaoPikaSeca', 'banana321');
-insert into login(username, senha) values ('TIgas', 'cacete321');
