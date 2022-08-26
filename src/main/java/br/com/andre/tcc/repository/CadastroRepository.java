9cd7af7b58f205a1fb43a7bc1c4737f8b496c404
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-26 09:22:27 -0300

    Delete CadastroRepository.java

--- src/main/java/br/com/andre/tcc/repository/CadastroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.andre.tcc.repository;
-
-import br.com.andre.tcc.model.Cadastro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
-}
