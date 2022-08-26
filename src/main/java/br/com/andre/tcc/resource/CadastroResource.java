55620150703d4de8dc7b992e4ce33f6f810c6b35
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-08-26 09:22:57 -0300

    Delete CadastroResource.java

--- src/main/java/br/com/andre/tcc/resource/CadastroResource.java
+++
@@ -1,9 +0,0 @@
-package br.com.andre.tcc.resource;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/cadastro")
-public class CadastroResource {
-}
