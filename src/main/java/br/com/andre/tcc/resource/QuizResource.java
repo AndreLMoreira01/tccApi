4c8c4bdd449edafb2755ce6c44d3265678eb8fc2
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-09-08 11:53:07 -0300

    Quiz Inserts

--- src/main/java/br/com/andre/tcc/resource/QuizResource.java
+++ src/main/java/br/com/andre/tcc/resource/QuizResource.java
@@ -1,10 +1,25 @@
 package br.com.andre.tcc.resource;
 
 
+import br.com.andre.tcc.model.Quiz;
+import br.com.andre.tcc.service.HistoriaService;
+import br.com.andre.tcc.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
 public class QuizResource {
+
+    @Autowired
+    private QuizService quizService;
+
+    @Autowired
+    private HistoriaService historiaService;
+
+
 }
---
+++ src/main/resources/db/migration/V004__Criar_Tabela_Quiz.sql
@@ -0,0 +1,94 @@
+ create table quiz(
+	id_quiz Bigint not null primary key auto_increment,
+	pergunta VARCHAR(100)
+ );
+
+ insert into quiz(pergunta)
+ values ('O menino comprou maçã');
+
+ insert into quiz(pergunta)
+ values ('A menina comprou uva');
+
+insert into quiz(pergunta)
+values ('Ele comprou morango');
+
+insert into quiz(pergunta)
+values ('Lucas comeu banana');
+
+insert into quiz(pergunta)
+values ('Marcela comeu kiwi');
+
+insert into quiz(pergunta)
+values ('Bianca comprou melancia');
+
+insert into quiz(pergunta)
+values ('A menina comeu abacaxi');
+
+insert into quiz(pergunta)
+values ('Tiago gosta de banana');
+
+insert into quiz(pergunta)
+values ('Carlos comeu uva');
+
+insert into quiz(pergunta)
+values ('Breno gosta de tomate');
+
+insert into quiz(pergunta)
+values ('Douglas comeu manga');
+
+insert into quiz(pergunta)
+values ('Eu gosto de limão');
+
+insert into quiz(pergunta)
+values ('A menina comprou pêra');
+
+insert into quiz(pergunta)
+values ('Mariana comeu laranja');
+
+insert into quiz(pergunta)
+values ('Nick comprou pêssego');
+
+insert into quiz(pergunta)
+values ('Marilia colheu mamão');
+
+insert into quiz(pergunta)
+values ('Ela gosta de cereja');
+
+insert into quiz(pergunta)
+values ('Marcela comprou amora');
+
+insert into quiz(pergunta)
+values ('Gustavo gosta de caqui');
+
+insert into quiz(pergunta)
+values ('Ele plantou limão');
+
+insert into quiz(pergunta)
+values ('Eu gosto de abacate');
+
+insert into quiz(pergunta)
+values ('A menina comprou maçã');
+
+insert into quiz(pergunta)
+values ('Ele comprou kiwi');
+
+insert into quiz(pergunta)
+values ('O menino gosta de manga');
+
+insert into quiz(pergunta)
+values ('André gosta de jabuticaba');
+
+insert into quiz(pergunta)
+values ('Ana plantou carambola');
+
+insert into quiz(pergunta)
+values ('Eu colhi goiaba');
+
+insert into quiz(pergunta)
+values ('Não gosto de ameixa');
+
+insert into quiz(pergunta)
+values ('Eu gosto de melão');
+
+insert into quiz(pergunta)
+values ('Luiz gosta de kiwi');
