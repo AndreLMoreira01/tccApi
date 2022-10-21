284ff0f610f7a1ec0a815c8471481f543fb80923
Luiz D'Avila <83254624+LuizDavilaa@users.noreply.github.com>
2022-10-21 09:40:01 -0300

    arrumando inserts - parte 1

--- src/main/resources/db/migration/V003__Criar_Tabela_Jogo.sql
+++ src/main/resources/db/migration/V003__Criar_Tabela_Jogo.sql
@@ -6,6 +6,6 @@
     conquista_id int not null
  );
 
+ insert into jogo(nomejogo, descricao, imagem, conquista_id) values ('Jogo da Velha', 'Desc', '../../assets/img/jogos/velha.png', 5);
  insert into jogo(nomejogo, descricao, imagem, conquista_id) values ('Jogo Dos Significados', 'Desc', '../../assets/img/jogos/significados.png', 3);
  insert into jogo(nomejogo, descricao, imagem, conquista_id) values ('Jogo das Frutas', 'Desc', '../../assets/img/jogos/frutas.png', 4);
- insert into jogo(nomejogo, descricao, imagem, conquista_id) values ('Jogo da Velha', 'Desc', '../../assets/img/jogos/velha.png', 5);
